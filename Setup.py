715664c580372509d21b32c7ced6cdb8752d9a14
Sajid Akhtar <sajid.akhtar9823@gmail.com>
2023-11-11 09:15:08 +0100

    Checking package creation

---
+++ Setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'A simple math quiz project'
+
+# Setting up
+setup(
+    name="mathquiz",
+    version=VERSION,
+    author="Sajid Akhtar (sajid-akhtar)",
+    author_email="<sajid.akhtar9823@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'math', 'math-quiz',
+              'assignment', 'fau-erlangan', 'ml'],
+    classifiers=[
+        "Development Status :: 1 - Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
